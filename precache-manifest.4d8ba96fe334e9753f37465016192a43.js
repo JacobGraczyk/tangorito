c96de9b34acdcef1cf55ccdeae842eef0356a6f0
Jacob Graczyk <jacobgraczyk@Jacobs-MacBook-Pro.local>
2020-04-22 16:37:46 -0400

    Updates

---
+++ precache-manifest.4d8ba96fe334e9753f37465016192a43.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d2d630b65187ebeada3cf46ab7c58b",
+    "url": "/tangorito/index.html"
+  },
+  {
+    "revision": "a1ade93ee16ff1bbb38e",
+    "url": "/tangorito/static/css/main.10884343.chunk.css"
+  },
+  {
+    "revision": "8cbd2529a21dc25bd86f",
+    "url": "/tangorito/static/js/2.4e6c16b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tangorito/static/js/2.4e6c16b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ade93ee16ff1bbb38e",
+    "url": "/tangorito/static/js/main.db01ddef.chunk.js"
+  },
+  {
+    "revision": "4ad78f0a48f532a185df",
+    "url": "/tangorito/static/js/runtime-main.bdc326d2.js"
+  },
+  {
+    "revision": "6d2ae696ab7d4926c6c9751c22ef2a2d",
+    "url": "/tangorito/static/media/breakfast-burritos.6d2ae696.jpg"
+  },
+  {
+    "revision": "56da5d8b1b26fbfa488ccc507eeb1190",
+    "url": "/tangorito/static/media/chicken-salad.56da5d8b.jpg"
+  },
+  {
+    "revision": "a3641e78051b3b9646017327dc5dfcfd",
+    "url": "/tangorito/static/media/chili-dog.a3641e78.jpg"
+  },
+  {
+    "revision": "ead9bf469a6cf5f8e355c0ab1a8394ac",
+    "url": "/tangorito/static/media/chili.ead9bf46.jpg"
+  },
+  {
+    "revision": "226dc46d2ad990d0be3663d349a1c173",
+    "url": "/tangorito/static/media/encheladas.226dc46d.jpg"
+  },
+  {
+    "revision": "cee51ad0f29a9c7e1f172d62446ec1b6",
+    "url": "/tangorito/static/media/guacamole.cee51ad0.jpg"
+  },
+  {
+    "revision": "0852eede0aec0cc6b1b7a1af7d476738",
+    "url": "/tangorito/static/media/logo.0852eede.svg"
+  },
+  {
+    "revision": "461f06c526904c11751f800bfaab895d",
+    "url": "/tangorito/static/media/meatballs.461f06c5.jpg"
+  },
+  {
+    "revision": "e10e0ec4326b9199a4c96d9eacd3a7c9",
+    "url": "/tangorito/static/media/nacho-chips.e10e0ec4.jpg"
+  },
+  {
+    "revision": "674915b013427d38d50992302f63ef58",
+    "url": "/tangorito/static/media/tacos.674915b0.jpg"
+  },
+  {
+    "revision": "6a4c8503fdbe86c017c882636a10a86a",
+    "url": "/tangorito/static/media/texas-steak.6a4c8503.jpg"
+  },
+  {
+    "revision": "62de0fe9298aaff411d952232bf411cb",
+    "url": "/tangorito/static/media/western-burger.62de0fe9.jpg"
+  },
+  {
+    "revision": "fcae86f92f20ecca63508bd46574e1ce",
+    "url": "/tangorito/static/media/white-queso.fcae86f9.jpg"
+  }
+]);
